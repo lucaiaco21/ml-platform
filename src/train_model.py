372b013ed30340c8b0b0ac24d1ae392601fc29ff
Luca Iacomino <lucaiacomino@MacBook-Pro.local>
2026-05-09 11:41:55 +0200

    Add Optuna Hyperparameter

--- src/train_model.py
+++ src/train_model.py
@@ -4,22 +4,45 @@ from sklearn.metrics import classification_report
 from data import load_data
 from features import prepare_features
 from xgboost import XGBClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
 import joblib
 import os
+import optuna
+from sklearn.model_selection import StratifiedKFold
 
 
+def objective(x, y, trial):
+    params = {
+        'n_estimators': trial.suggest_int('n_estimators', 50, 300),
+        'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3),
+        'max_depth': trial.suggest_int('max_depth', 3, 10),
+    }
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
 
+    weight = y.value_counts()[0] / y.value_counts()[1]
 
-
-def train_model(x, y):
+    model = XGBClassifier(scale_pos_weight=weight, **params)
+  
     
-    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    scores = cross_val_score(model, X_train, Y_train, cv=kf, scoring='f1', n_jobs=-1)
+    return scores.mean()
 
-    model = XGBClassifier(n_estimators=100, random_state=42)
+
+
+def train_model(x, y, best_params):
+    
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
+    weight = y.value_counts()[0] / y.value_counts()[1]
+    param = best_params
+    model = XGBClassifier(scale_pos_weight=weight, **param)
+    cv_scores = cross_val_score(model, X_train, Y_train, cv=5, scoring="f1")
     model.fit(X_train, Y_train)
 
     Y_pred = model.predict(X_test)
     print(classification_report(Y_test, Y_pred))
+    print("Cross-validation F1 scores:", cv_scores)
     os.makedirs("models", exist_ok=True)
     joblib.dump(model, "models/xgb_model.joblib")
 
@@ -27,4 +50,8 @@ def train_model(x, y):
 if __name__ == "__main__":
     df = load_data("data/raw/churn.csv")
     x, y = prepare_features(df)
-    train_model(x,y)
+    study = optuna.create_study(direction='maximize')
+    study.optimize(lambda trial: objective(x, y, trial), n_trials=100, timeout=600)
+    best_params = study.best_params
+    print("Best Hyperparameters:", best_params)
+    train_model(x, y, best_params)
